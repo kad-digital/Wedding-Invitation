91868dbdae3c9125cbc2bca316c3c251bf5d4c45
kad-digital <98181459+kad-digital@users.noreply.github.com>
2022-01-22 17:08:34 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /**
  * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
  */
+ 
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
@@ -20,6 +21,7 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
+
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
@@ -44,12 +46,12 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    // document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
     
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+        // document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
     }
 }, 1000);
 
